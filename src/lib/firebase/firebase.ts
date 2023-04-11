e423126000f3af7840c81151578f6e75759e8104
David Kort <kortdk@icloud.com>
2023-04-11 14:34:58 +0200

    Init firebase

---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBlnCm9gtNueFGEb_CkqtFD7Gcj_yY1M0",
+  authDomain: "isef23-quiz.firebaseapp.com",
+  projectId: "isef23-quiz",
+  storageBucket: "isef23-quiz.appspot.com",
+  messagingSenderId: "462330911237",
+  appId: "1:462330911237:web:9b7ef2926b3d07928a9a6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
