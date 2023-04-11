80e6441bf6f49be10f87f21973f85df0bfe8d770
David Kort <kortdk@icloud.com>
2023-04-11 14:53:01 +0200

    Init Firestore and Firebase Auth services

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
+import { getAuth, setPersistence, browserSessionPersistence } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Initialize Authentication
+const auth = getAuth(app);
+setPersistence(auth, browserSessionPersistence);
