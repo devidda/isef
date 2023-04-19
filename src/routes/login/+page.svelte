246fcdd013e4c2f6b82daf70e7538630d5f738f9
David Kort <kortdk@icloud.com>
2023-04-19 12:33:00 +0200

    Add basic login and signup functions with html templates

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Signup from "./signup.svelte";
+  import Login from "./login.svelte";
+
+  let wantsToSignup: boolean = false;
+
+  function toggleLoginMode(event: MouseEvent) {
+    wantsToSignup = !wantsToSignup;
+  } 
+</script>
+
+<html lang="en">
+  <body>
+    <h1>Login</h1>
+
+    <div>
+      {#if wantsToSignup}
+        <Signup/>
+      {:else}
+        <Login/>
+      {/if}
+      <div>
+        <button on:click={toggleLoginMode}>
+          {#if wantsToSignup}
+            login instead
+          {:else}
+            signup instead
+          {/if}
+        </button>
+      </div>
+    </div>
+  </body>
+</html>
+
+<style>
+  html,body {
+    height:100%;
+    width:100%;
+    margin:0;
+  }
+  body {
+    display:flex;
+  }
+  div {
+    margin:auto;
+  }
+</style>
---
+++ src/routes/login/signup.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { signup } from "$lib/auth";
+
+  let username: string;
+  let email: string;
+  let password: string;
+</script>
+
+<form
+  on:submit|preventDefault={(event) => signup(event, username, email, password)}
+>
+  <label for="username">Username:</label>
+  <input type="username" id="username" bind:value={username} required />
+  <br />
+  <label for="email">Email:</label>
+  <input type="email" id="email" bind:value={email} required />
+  <br />
+  <label for="password">Password:</label>
+  <input type="password" id="password" bind:value={password} required />
+  <br />
+  <button type="submit">sign up</button>
+</form>
+
