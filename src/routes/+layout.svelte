2fb2d414d443a77f04c0626383589027889c7162
KeiraShadow <100292970+KeiraShadow@users.noreply.github.com>
2023-04-26 17:16:32 +0200

    Css changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,26 @@
   const user = userStore(auth);
 </script>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-  <a href="/login">Login</a>
-  {#if $user}
-    <button on:click={signOut}>Sign Out</button>
-  {/if}
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="/login">Login</a>
+      </li> 
+      {#if $user}
+      <li class="nav-item">
+        <button class="btn btn-outline-secondary" on:click={signOut}>Sign Out</button>
+      </li>
+      {/if}
+   </ul>
+  </div>
 </nav>
 
 <slot></slot>
