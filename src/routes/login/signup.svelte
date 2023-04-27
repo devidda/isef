2067fb713ee3b99abe287bceb797481e05902d2b
Quarknase0815 <spitzner87@gmail.com>
2023-04-27 09:57:40 +0200

    check for iu mail address

--- src/routes/login/signup.svelte
+++ src/routes/login/signup.svelte
@@ -4,15 +4,33 @@
   let username: string;
   let email: string;
   let password: string;
+  
+  function isValidEmail(email: string): boolean {
+    return email.endsWith("@iu-study.org") || email.endsWith("@iubh-fernstudium.de") || email.endsWith("@iubh.de") || email.endsWith("@iu.org");
+  }
+
+  function passwordLength(password: string): boolean {
+    return (password.length < 6);
+  }
 </script>
 
 <form
-  on:submit|preventDefault={(event) => signup(event, username, email, password)}
+  on:submit|preventDefault={(event) => {
+    if (!isValidEmail(email)) {
+      alert("No valid IU Mail-address");
+      return;
+    }
+    if (passwordLength(password)) {
+      alert("Password should be at least 6 characters");
+      return;
+    }
+    signup(event, username, email, password);
+  }}
 >
   <label for="username">Username:</label>
   <input type="username" id="username" bind:value={username} required />
   <br />
-  <label for="email">Email:</label>
+  <label for="email">IU Email:</label>
   <input type="email" id="email" bind:value={email} required />
   <br />
   <label for="password">Password:</label>
