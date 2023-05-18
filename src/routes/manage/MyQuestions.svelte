5acfd8c33898f9b5984bbef077c0c30c76303648
KeiraShadow <100292970+KeiraShadow@users.noreply.github.com>
2023-05-18 18:53:26 +0200

    change

--- src/routes/manage/MyQuestions.svelte
+++ src/routes/manage/MyQuestions.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import { db } from '$lib/firebase/firebase';
+    import { collection, getDocs } from 'firebase/firestore';
   
-    onMount(() => {
-      // Add your Firestore query or logic here to fetch and display the user's questions
-      // You can use the "db" variable imported from "$lib/firebase/firebase" to access Firestore
-      // Example:
-      // const querySnapshot = await getDocs(collection(db, 'QuizQuestions'));
-      // const questions = querySnapshot.docs.map((doc) => doc.data());
-      // console.log(questions);
+    let questions = [];
+     
+    
+
+    onMount(async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'QuizQuestions'));
+        questions = querySnapshot.docs.map((doc) => doc.data());
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
     });
   </script>
   
   <h1>My Questions Page</h1>
   <!-- Add your markup and logic to display the user's questions here -->
+  {#if questions.length > 0}
+    <ul>
+      {#each questions as question}
+        <li>{question.question}</li>
+      {/each}
+    </ul>
+  {:else}
+    <p>No questions found.</p>
+  {/if}
   
