df398baff6eea8ae56de29f44528f3e98b6ddcf6
KeiraShadow <100292970+KeiraShadow@users.noreply.github.com>
2023-05-18 19:31:50 +0200

    Update redirect.ts

--- src/routes/manage/redirect.ts
+++ src/routes/manage/redirect.ts
@@ -2,10 +2,17 @@
 
 // Import the `goto` function from Svelte Router
 import { goto } from '$app/navigation';
+import QuizList from './QuizList.svelte';
+import { router } from '$app/navigation';
 
+// Define the `/QuizList` route
+router.add('/QuizList', QuizList);
+
+// Define the `redirectQuizList` function
 function redirectQuizList() {
-  // Use the `goto` function to navigate to the QuizList route
+  // Use the `goto` function to navigate to the `/QuizList` route
   goto('/QuizList');
 }
 
 export { redirectQuizList };
+
