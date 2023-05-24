6ff861b19a7b8cdc632d93d12232ba5f5005a2e0
Quarknase0815 <spitzner87@gmail.com>
2023-05-24 16:16:22 +0200

    finished implementation

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,127 +1,104 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import { auth, db } from "$lib/firebase/firebase";
-  import { userStore } from 'sveltefire';
-  import { collection, addDoc, doc, deleteDoc, updateDoc, Timestamp } from 'firebase/firestore';
-  import { Button, FormGroup, Label, Input } from 'sveltestrap';
-  import "$lib/main.css";
-  import type { quiz } from "$lib/utils/db.d.ts";
+	import { goto } from '$app/navigation';
+	import { auth, db } from '$lib/firebase/firebase';
+	import { userStore } from 'sveltefire';
+	import { collection, addDoc, doc, deleteDoc, updateDoc, Timestamp } from 'firebase/firestore';
+	import { Button, FormGroup, Label, Input } from 'sveltestrap';
+	import '$lib/main.css';
+	import type { quiz } from '$lib/utils/db.d.ts';
 
-  function redirectLogin(event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement; }): any {
-    goto("/login")
+	function redirectLogin(
+		event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }
+	): any {
+		goto('/login');
 	}
 
-  let showMyQuestions = false;
+	const user = userStore(auth);
+	let quizQuestion: quiz = {
+		correctAnswer: '',
+		difficulty: '',
+		lastModified: Timestamp.now(),
+		listOfFalseAnswers: ['', '', ''],
+		question: '',
+		tags: [''],
+		id: '',
+		creatorID: ''
+	};
+	let editingQuestion: quiz | null = null;
 
-  function toggleShowMyQuestions() {
-    showMyQuestions = !showMyQuestions;
-  }
-  
-  const user = userStore(auth);
-  let quizQuestion : quiz = { correctAnswer:'', difficulty:'', lastModified:Timestamp.now(), listOfFalseAnswers:['','',''], question:'', tags:[''], id:'', creatorID:'' };
-  let editingQuestion: quiz | null = null;
-  
-  async function createQuizQuestion() {
-        
-    quizQuestion.lastModified = Timestamp.now();
-    try {
-      if (editingQuestion) {
-        // Update existing question
-        const questionRef = doc(db, 'quiz', editingQuestion.id);
-        await updateDoc(questionRef, {
-          'correctAnwser': quizQuestion.correctAnswer,
-          'difficulty': quizQuestion.difficulty,
-          'lastModified': quizQuestion.lastModified,
-          'listOfFalseAnswers': quizQuestion.listOfFalseAnswers,
-          'question': quizQuestion.question,
-          'tags': quizQuestion.tags
-        });
-        console.log('Quiz question updated with ID:', editingQuestion.id);
-        editingQuestion = null;
-      } else {
-        // Create new question
-        if ($user !== null){      
-        console.log('Question:', quizQuestion.question);
-        console.log('listOfFalseAnswers:', quizQuestion.listOfFalseAnswers);
-        const docRef = await addDoc(collection(db, 'quiz'), {
-          'correctAnwser': quizQuestion.correctAnswer,
-          'difficulty': quizQuestion.difficulty,
-          'lastModified': quizQuestion.lastModified,
-          'listOfFalseAnswers': quizQuestion.listOfFalseAnswers,
-          'question': quizQuestion.question,
-          'tags': quizQuestion.tags,
-          'creatorID': $user.uid
-        });
-        console.log('Quiz question created with ID:', docRef.id);
-      }}
-      resetForm();
-    } catch (error) {
-      console.error('Error creating/updating quiz question:', error);
-    }
-  }
+	async function createQuizQuestion() {
+		quizQuestion.lastModified = Timestamp.now();
+		try {
+			// Create new question
+			if ($user !== null) {
+				console.log('Question:', quizQuestion.question);
+				console.log('listOfFalseAnswers:', quizQuestion.listOfFalseAnswers);
+				const docRef = await addDoc(collection(db, 'quiz'), {
+					correctAnswer: quizQuestion.correctAnswer,
+					difficulty: quizQuestion.difficulty,
+					lastModified: quizQuestion.lastModified,
+					listOfFalseAnswers: quizQuestion.listOfFalseAnswers,
+					question: quizQuestion.question,
+					tags: quizQuestion.tags,
+					creatorID: $user.uid
+				});
+				console.log('Quiz question created with ID:', docRef.id);
+			}
+			resetForm();
+		} catch (error) {
+			console.error('Error creating/updating quiz question:', error);
+		}
+	}
 
-  async function deleteQuizQuestion(questionId: string) {
-    try {
-      const questionRef = doc(db, 'quiz', questionId);
-      await deleteDoc(questionRef);
-      console.log('Quiz question deleted with ID:', questionId);
-    } catch (error) {
-      console.error('Error deleting quiz question:', error);
-    }
-  }
-
-  function resetForm() {
-    quizQuestion.question = '';
-    quizQuestion.listOfFalseAnswers = ['', '', ''];
-    quizQuestion.correctAnswer = '';
-    editingQuestion = null;
-  }
+	function resetForm() {
+		quizQuestion.question = '';
+		quizQuestion.listOfFalseAnswers = ['', '', ''];
+		quizQuestion.correctAnswer = '';
+		editingQuestion = null;
+	}
 </script>
 
 <html lang="en">
-  <body>
-    <div class="container-background">
-      <div class="card text-black ">
-        <div class="card-header">
-          <h1>Quiz Questions</h1>
-          
-        </div>       
-        {#if $user !== null}
-            <p>Logged in as: {$user.uid}</p>
-            <p>With: {$user.email}</p>
-        {:else}
-            <p>Not logged in</p>
-            <button class="btn btn-light" on:click={redirectLogin}>Let me in! 🚀</button>
-        {/if}
+	<body>
+		<div class="container-background">
+			<div class="card text-black">
+				<div class="card-header">
+					<h1>Quiz Questions</h1>
+				</div>
+				{#if $user !== null}
+					<main>
+						<h3>Create your own Quiz Questions</h3>
 
-        {#if $user !== null}
-          <main>
-            <h3>Create your own Quiz Questions</h3>
+						<form on:submit|preventDefault={createQuizQuestion}>
+							<FormGroup>
+								<Label for="question">Question:</Label>
+								<Input type="text" id="question" bind:value={quizQuestion.question} />
+							</FormGroup>
 
-            <form on:submit|preventDefault="{createQuizQuestion}">
-              <FormGroup>
-                <Label for="question">Question:</Label>
-                <Input type="text" id="question" bind:value="{quizQuestion.question}" />
-              </FormGroup>
+							<FormGroup>
+								<Label for="correctAnswer">Correct Answer:</Label>
+								<Input type="text" id="correctAnswer" bind:value={quizQuestion.correctAnswer} />
+							</FormGroup>
 
-              <FormGroup>
-                <Label for="correctAnswer">Correct Answer:</Label>
-                <Input type="text" id="correctAnswer" bind:value="{quizQuestion.correctAnswer}" />
-              </FormGroup>
-
-              {#each quizQuestion.listOfFalseAnswers as option, index}
-                <FormGroup>
-                  <Label for="{`option${index}`}">False Answer {index + 1}:</Label>
-                  <Input type="text" id="{`option${index}`}" bind:value="{quizQuestion.listOfFalseAnswers[index]}" />
-                </FormGroup>
-              {/each}
-                <Button type="submit" color="primary">Create Quiz Question</Button>
-            </form>
-          </main>
-        {:else}
-          <p>Please log in to create your own quiz questions.</p>
-        {/if}
-      </div>
-    </div>
-  </body>
+							{#each quizQuestion.listOfFalseAnswers as option, index}
+								<FormGroup>
+									<Label for={`option${index}`}>False Answer {index + 1}:</Label>
+									<Input
+										type="text"
+										id={`option${index}`}
+										bind:value={quizQuestion.listOfFalseAnswers[index]}
+									/>
+								</FormGroup>
+							{/each}
+							<Button type="submit" color="primary">Create Quiz Question</Button>
+						</form>
+					</main>
+				{:else}
+					<p>Not logged in</p>
+					<p>Please log in to create your own quiz questions.</p>
+					<button class="btn btn-light" on:click={redirectLogin}>Let me in! 🚀</button>
+				{/if}
+			</div>
+		</div>
+	</body>
 </html>
--- src/routes/manage/+page.svelte
+++ src/routes/manage/+page.svelte
@@ -2,26 +2,70 @@
 	import { goto } from '$app/navigation';
 	import { auth, db } from '$lib/firebase/firebase';
 	import { userStore, collectionStore } from 'sveltefire';
-	import { collection, getCountFromServer, addDoc, doc, deleteDoc, updateDoc, Timestamp } from 'firebase/firestore';
+	import { collection, getCountFromServer, doc, deleteDoc, updateDoc, Timestamp } from 'firebase/firestore';
 	import { Button, FormGroup, Label, Input } from 'sveltestrap';
-	import '$lib/main.css';
 	import type { quiz } from '$lib/utils/db.d.ts';
-
+	import '$lib/main.css';
+	
 	function redirectLogin(
 		event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }
 	): any {
 		goto('/login');
 	}
 
-	let showMyQuestions = false;
-	let editField: any;
+	//#region updated quiz questions
+	let updatedQuestion: string;
+	let updatedQuestionID: string;
+	let updatedCorrectAnswer: string;
+	let updatedFalseAnswer0: string;
+	let updatedFalseAnswer1: string;
+	let updatedFalseAnswer2: string;
 
-	function toggleShowMyQuestions() {
-		showMyQuestions = !showMyQuestions;
+	function initializeUpdatedQuestion(actualstring: string) {
+		updatedQuestion = actualstring;
 	}
+
+	function handleInputChangeUpdatedQuestion(event: any) {
+		updatedQuestion = event.target.value;
+	}
+
+	function initializeUpdatedQuestionID(actualstring: string) {
+		updatedQuestionID = actualstring;
+	}
+
+	function initializeUpdatedCorrectAnswer(actualstring: string) {
+		updatedCorrectAnswer = actualstring;
+	}
+	function handleInputChangeUpdatedCorrectAnswer(event: any) {
+		updatedCorrectAnswer = event.target.value;
+	}
+
+	function initializeUpdatedFalseAnswer0(actualstring: string) {
+		updatedFalseAnswer0 = actualstring;
+	}
+	function handleInputChangeUpdatedFalseAnswer0(event: any) {
+		updatedFalseAnswer0 = event.target.value;
+	}
+
+	function initializeUpdatedFalseAnswer1(actualstring: string) {
+		updatedFalseAnswer1 = actualstring;
+	}
+	function handleInputChangeUpdatedFalseAnswer1(event: any) {
+		updatedFalseAnswer1 = event.target.value;
+	}
+
+	function initializeUpdatedFalseAnswer2(actualstring: string) {
+		updatedFalseAnswer2 = actualstring;
+	}
+	function handleInputChangeUpdatedFalseAnswer2(event: any) {
+		updatedFalseAnswer2 = event.target.value;
+	}
+	//#endregion
+
 	const existingQuizQuestions = collectionStore<quiz>(db, 'quiz');
 	const existingQuizQuestionsForSize = collection(db, 'quiz');
 	const user = userStore(auth);
+	let editField: any;
 	let quizQuestion: quiz = {
 		correctAnswer: '',
 		difficulty: '',
@@ -32,7 +76,6 @@
 		id: '',
 		creatorID: ''
 	};
-	let editingQuestion: quiz | null = null;
 
 	async function initializeEditField() {
 		editField = [];
@@ -45,41 +88,33 @@
 
 	initializeEditField();
 
-	function helperfunction() {}
-	async function createQuizQuestion() {
-		quizQuestion.lastModified = Timestamp.now();
-		try {
-			if (editingQuestion) {
-				// Update existing question
-				const questionRef = doc(db, 'quiz', editingQuestion.id);
-				await updateDoc(questionRef, {
-					correctAnswer: quizQuestion.correctAnswer,
-					difficulty: quizQuestion.difficulty,
-					lastModified: quizQuestion.lastModified,
-					listOfFalseAnswers: quizQuestion.listOfFalseAnswers,
-					question: quizQuestion.question,
-					tags: quizQuestion.tags
-				});
-				console.log('Quiz question updated with ID:', editingQuestion.id);
-				editingQuestion = null;
-			} else {
-				// Create new question
-				if ($user !== null) {
-					console.log('Question:', quizQuestion.question);
-					console.log('listOfFalseAnswers:', quizQuestion.listOfFalseAnswers);
-					const docRef = await addDoc(collection(db, 'quiz'), {
-						correctAnswer: quizQuestion.correctAnswer,
-						difficulty: quizQuestion.difficulty,
-						lastModified: quizQuestion.lastModified,
-						listOfFalseAnswers: quizQuestion.listOfFalseAnswers,
-						question: quizQuestion.question,
-						tags: quizQuestion.tags,
-						creatorID: $user.uid
-					});
-					console.log('Quiz question created with ID:', docRef.id);
+	function toggleEditField(index: number) {
+		if (editField[index] === true) {
+			editField[index] = false;
+		} else {
+			for (let i = 0; i < editField.length; i++) {
+				if (editField[i] === true) {
+					editField[i] = false;
 				}
 			}
+			editField[index] = true;
+		}
+	}
+
+	async function updateQuizQuestion() {
+		quizQuestion.lastModified = Timestamp.now();
+		try {
+			const questionRef = doc(db, 'quiz', updatedQuestionID);
+			await updateDoc(questionRef, {
+				correctAnswer: updatedCorrectAnswer,
+				lastModified: quizQuestion.lastModified,
+				listOfFalseAnswers: [updatedFalseAnswer0, updatedFalseAnswer1, updatedFalseAnswer2],
+				question: updatedQuestion
+			});
+
+			console.log('Quiz question updated with ID:', updatedQuestionID);
 			resetForm();
+
 		} catch (error) {
 			console.error('Error creating/updating quiz question:', error);
 		}
@@ -92,6 +127,13 @@
 			console.log('Quiz question deleted with ID:', questionId);
 		} catch (error) {
 			console.error('Error deleting quiz question:', error);
+
+			//After delete -> close all open questions
+			for (let i = 0; i < editField.length; i++) {
+				if (editField[i] === true) {
+					editField[i] = false;
+				}
+			}
 		}
 	}
 
@@ -99,7 +141,6 @@
 		quizQuestion.question = '';
 		quizQuestion.listOfFalseAnswers = ['', '', ''];
 		quizQuestion.correctAnswer = '';
-		editingQuestion = null;
 	}
 </script>
 
@@ -108,58 +149,94 @@
 		<div class="container-background">
 			<div class="card text-black">
 				<div class="card-header">
-					<h1>Edit and delete your own Quiz Questions</h1>
+					<h1>Edit and delete Quiz Questions</h1>
 				</div>
-				{#if $user !== null}
-					<!--<p>Logged in as: {$user.uid}</p>
-            <p>With: {$user.email}</p>-->
-				{:else}
-					<p>Not logged in</p>
-					<button class="btn btn-light" on:click={redirectLogin}>Let me in! 🚀</button>
-				{/if}
 
 				{#if $user !== null}
 					<main>
-						<h3>Please select the question that you want to edit or delete</h3>
+						<h3>Feel free to edit or delete your own Questions</h3>
 						<br />
 						{#each $existingQuizQuestions as quizQuestion, index}
 							{#if quizQuestion.creatorID === $user.uid}
 								<p>
-									<Button color="danger" on:click={() => deleteQuizQuestion(quizQuestion.id)} >Delete</Button>
-									<Button color="primary" on:click={() => (editField[index] = !editField[index])}>Edit</Button>
+									<Button color="danger" on:click={() => deleteQuizQuestion(quizQuestion.id)}
+										>Delete</Button
+									>
+									<Button color="primary" on:click={() => toggleEditField(index)}>Edit</Button>
 									{quizQuestion.question}
 								</p>
 								{#if editField[index]}
+									<p style="display: none;">
+										This is a special workaround... I'm not a JS-developer...
+										{initializeUpdatedQuestion(quizQuestion.question)}
+										{initializeUpdatedQuestionID(quizQuestion.id)}
+										{initializeUpdatedCorrectAnswer(quizQuestion.correctAnswer)}
+										{initializeUpdatedFalseAnswer0(quizQuestion.listOfFalseAnswers[0])}
+										{initializeUpdatedFalseAnswer1(quizQuestion.listOfFalseAnswers[1])}
+										{initializeUpdatedFalseAnswer2(quizQuestion.listOfFalseAnswers[2])}
+									</p>
+									<FormGroup>
+										<Label for="question">Question:</Label>
+										<Input
+											type="text"
+											id="question"
+											value={quizQuestion.question}
+											on:input={handleInputChangeUpdatedQuestion}
+										/>
+									</FormGroup>
 
-                <form on:submit|preventDefault={createQuizQuestion}>
-                  <FormGroup>
-                    <Label for="question">Questionl:</Label>
-                    <Input type="text" id="question" bind:value={quizQuestion.question} />
-                  </FormGroup>
-    
-                  <FormGroup>
-                    <Label for="correctAnswer">Correct Answer:</Label>
-                    <Input type="text" id="correctAnswer" bind:value={quizQuestion.correctAnswer} />
-                  </FormGroup>
-    
-                  {#each quizQuestion.listOfFalseAnswers as option, index}
-                    <FormGroup>
-                      <Label for={`option${index}`}>False Answer {index + 1}:</Label>
-                      <Input
-                        type="text"
-                        id={`option${index}`}
-                        bind:value={quizQuestion.listOfFalseAnswers[index]}
-                      />
-                    </FormGroup>
-                  {/each}
-                    <Button type="submit" color="primary">Update Quiz Question</Button>
-                </form>
+									<FormGroup>
+										<Label for="correctAnswer">Correct Answer:</Label>
+										<Input
+											type="text"
+											id="correctAnswer"
+											value={quizQuestion.correctAnswer}
+											on:input={handleInputChangeUpdatedCorrectAnswer}
+										/>
+									</FormGroup>
+
+									<FormGroup>
+										<Label for="False Answer 1">False Answer 1:</Label>
+										<Input
+											type="text"
+											id="option1"
+											value={quizQuestion.listOfFalseAnswers[0]}
+											on:input={handleInputChangeUpdatedFalseAnswer0}
+										/>
+									</FormGroup>
+
+									<FormGroup>
+										<Label for="False Answer 2">False Answer 2:</Label>
+										<Input
+											type="text"
+											id="option2"
+											value={quizQuestion.listOfFalseAnswers[1]}
+											on:input={handleInputChangeUpdatedFalseAnswer1}
+										/>
+									</FormGroup>
+
+									<FormGroup>
+										<Label for="False Answer 3">False Answer 3:</Label>
+										<Input
+											type="text"
+											id="option3"
+											value={quizQuestion.listOfFalseAnswers[2]}
+											on:input={handleInputChangeUpdatedFalseAnswer2}
+										/>
+									</FormGroup>
+
+									<Button on:click={() => updateQuizQuestion()} color="primary"
+										>Update Quiz Question</Button
+									>
+									<br /><br />
 								{/if}
 							{/if}
 						{/each}
 					</main>
 				{:else}
+					<p>Not logged in</p>
 					<p>Please log in to create your own quiz questions.</p>
+					<button class="btn btn-light" on:click={redirectLogin}>Let me in! 🚀</button>
 				{/if}
 			</div>
 		</div>
