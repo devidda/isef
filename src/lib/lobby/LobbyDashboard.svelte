8528bf784215801ffed5942cc585484a601f8b36
David Kort <kortdk@icloud.com>
2023-05-28 14:57:21 +0200

    Fix diplaying bugs when joining lobby

--- src/lib/lobby/LobbyDashboard.svelte
+++ src/lib/lobby/LobbyDashboard.svelte
@@ -45,14 +45,23 @@
     </div>
     <div class="settings-container">
       <h1>Settings:</h1>
-      <FormGroup>
-        <Label for="gameModes">Game modes:</Label>
-        <Input type="select" name="gameModes" id="gameModes" bind:value={selectedMode} disabled={activeLobby.listOfUsers[0] === loggedInUser.uid ? false: true}>
-          {#each gameModes as mode}
-            <option value={mode}>{mode}</option>
-          {/each}
-        </Input>
-      </FormGroup>
+      {#if activeLobby.listOfUsers[0] === loggedInUser.uid}
+        <FormGroup>
+          <Label for="gameModes">Select Game Mode (Versus-mode currently not implemented):</Label>
+          <Input type="select" name="gameModes" id="gameModes" bind:value={selectedMode} disabled={activeLobby.listOfUsers[0] === loggedInUser.uid ? false: true}>
+            {#each gameModes as mode}
+              <option value={mode}>{mode}</option>
+            {/each}
+          </Input>
+        </FormGroup>
+      {:else}
+        <FormGroup>
+          <Label for="gameModeSelected">Selected Game Mode by Lobby Owner:</Label>
+          <div>
+            <Button id="gameModeSelected" disabled={true}>{activeLobby.gameMode}</Button>
+          </div>
+        </FormGroup>
+      {/if}
       {#if activeLobby.listOfUsers[0] === loggedInUser.uid}
         <p>Enter time limit per question between 6 and 30 seconds:</p>
         <Input type="number" bind:value={timeLimit} min=6 max=30/>
@@ -71,12 +80,23 @@
         <Button class="start-game-button" color="primary" on:click={() => saveSettings()} disabled={activeLobby.listOfUsers[0] === loggedInUser.uid ? false: true}>Save Settings</Button>
         </Collection>
       {:else}
+        <FormGroup>
+          <Label for="timeLimitSelected">Time limit per question selected by Lobby Owner:</Label>
+          <div>
+            <Button id="timeLimitSelected" disabled={true}>{activeLobby.timeLimit}</Button>
+          </div>
+        </FormGroup>
         <p>These Question Stacks are currently selected by the lobby owner:</p>
         <ListGroup>
           {#each activeLobby.questionStacks as qStack}
-            <ListGroupItem>{qStack}</ListGroupItem>
+            <Doc ref={`stack/${qStack}`} let:data={stackData}>
+            <ListGroupItem>{stackData.name}</ListGroupItem>
+            </Doc>
           {/each}
         </ListGroup>
+        {#if activeLobby.questionStacks.length === 0}
+          <ListGroupItem>Nothing selected yet.</ListGroupItem>
+        {/if}
       {/if}
     </div>
   </Doc>
